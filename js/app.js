699f93401f7bb90ea97318beaa48520ea23707a3
hachinrayhan <rayhan.hachin@gmail.com>
2022-08-23 11:36:24 +0600

    ol created

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.getElementById('players').addEventListener('click', function (event) {
+    if (event.target.innerText === 'SELECT') {
+        const currentNode = event.target;
+        const preNode = currentNode.previousElementSibling;
+        const player = preNode.innerText;
+        const ol = document.getElementById('player-list');
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(player));
+        ol.appendChild(li);
+    }
+})
